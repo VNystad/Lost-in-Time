738a5f842a58047c7ce141bbdba897b352ba4b08
Kristoffer Lomeland Magelssen <krislm15@student.uia.no>
2016-11-24 10:00:27 +0100

    Changed void Animation() in AI.h to AnimationAI()

---
+++ Project-Enjoy/Lost-in-Time/cmake-build-debug/CMakeFiles/Lost-in-Time.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lost-in-Time.dir/main.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/testapp.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Enemy/AI.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Enemy/AIEnemies.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Framework/animation.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Framework/physics.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Framework/font.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Interface/health.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Map/layer.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Map/map.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Map/object.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Map/sprite.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/Objects/playertest.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/state/loadgamestate.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/state/machine.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/state/stategame.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/state/statemainmenu.cpp.o"
+  "CMakeFiles/Lost-in-Time.dir/state/statepause.cpp.o"
+  "Lost-in-Time.pdb"
+  "Lost-in-Time"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lost-in-Time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
